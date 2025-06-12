1d67c102c62c1f05eff2bd55fef32a2b6de19110
huouvcti <huouvcti@gamil.com>
2025-06-12 10:56:25 +0900

    Lv 5. 공통 예외 로직 추출, 도메인 로직 분리
    UserService 메서드 추가
    - getUserByIdOrThrow()
    - getManagerByIdOrThrow()
    - getUserByEmailOrThrow()
    - ensureEmailIsUnique()
    - saveUser()
    
    TodoService 메서드 추가
    - getTodoOrThrow()
    
    - 메서드 사용

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -65,8 +65,7 @@ public class TodoService {
     }
 
     public TodoResponse getTodo(long todoId) {
-        Todo todo = todoRepository.findByIdWithUser(todoId)
-                .orElseThrow(() -> new InvalidRequestException("Todo not found"));
+        Todo todo = getTodoOrThrow(todoId);
 
         User user = todo.getUser();
 
@@ -79,5 +78,12 @@ public class TodoService {
                 todo.getCreatedAt(),
                 todo.getModifiedAt()
         );
+    }
+
+    /* 도메인 로직 분리 */
+
+    public Todo getTodoOrThrow(long todoId) {
+        return todoRepository.findByIdWithUser(todoId)
+                .orElseThrow(() -> new InvalidRequestException("Todo not found"));
     }
 }
